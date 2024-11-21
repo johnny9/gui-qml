2cc71dcb556bc8f004130c8ceb2bef5859fef549
jarolrod <jarolrod@tutanota.com>
2024-11-21 12:12:28 -0500

    qml: have NodeModel return formatted verification progress string
    Instead of having the UI compute the formatted version of verification
    progress using js, move this computation into the nodemodel; where it
    belongs.
    
    The UI is the UI, and the backend is the backend.

--- src/qml/models/nodemodel.cpp
+++ src/qml/models/nodemodel.cpp
@@ -75,19 +75,35 @@ void NodeModel::setRemainingSyncTime(double new_progress)
         }
     }
 }
+
 void NodeModel::setVerificationProgress(double new_progress)
 {
     if (new_progress != m_verification_progress) {
         setRemainingSyncTime(new_progress);
+        setFormattedVerificationProgress(new_progress);
 
         m_verification_progress = new_progress;
         Q_EMIT verificationProgressChanged();
     }
 }
 
+void NodeModel::setFormattedVerificationProgress(double new_progress)
+{
+    double percentage = new_progress * 100.00;
+
+    if (percentage >= 99.99) {
+        m_formatted_verification_progress = QStringLiteral("100%");
+    } else if (percentage == 0.0) {
+        m_formatted_verification_progress = QStringLiteral("0%");
+    } else {
+        int decimal_places = (percentage >= 10.0) ? 1 : 2;
+        m_formatted_verification_progress = QString::number(percentage, 'f', decimal_places) + '%';
+    }
+}
+
 void NodeModel::setPause(bool new_pause)
 {
-    if(m_pause != new_pause) {
+    if (m_pause != new_pause) {
         m_pause = new_pause;
         m_node.setNetworkActive(!new_pause);
         Q_EMIT pauseChanged(new_pause);
